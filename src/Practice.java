e6b548005f17f5649637a763e3fae68d6d9d422f
Rajesh <nol151740@gmail.com>
2024-06-13 18:24:40 +0545

    initial commit

--- src/Practice.java
+++ src/Practice.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 public class Practice {
-    public static void solveHanoi(int n , char source, char Temporary, char destination) {
+    public static void solveHanoi(int n, char source,char temporary, char destination) {
         if (n == 1) {
             System.out.println("Move disk 1 form"+source+"to"+destination);
             return;
         }
-        solveHanoi(n-1,source,destination,Temporary);
-        System.out.println("Move disk" +n+ "from" +source+ "to" +destination);
-        solveHanoi(n-1,Temporary,source,destination);
+        solveHanoi(n-1,source,destination,temporary);
+        System.out.println("Move disk"+n+"from"+source+ "to"+destination);
+        solveHanoi(n-1,temporary,source,destination);
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number of disk");
-        int NumberOfDisk = scanner.nextInt();
-        solveHanoi(NumberOfDisk, 'L' , 'C' , 'R' );
+        System.out.println("Enter the number of disk");
+
+        int numberofdisk = scanner.nextInt();
+        solveHanoi(numberofdisk, 'L', 'C', 'R');
+        scanner.close();
     }
 }
